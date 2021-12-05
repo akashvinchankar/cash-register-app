782018d325370ba83068c9b23bbee47ad111c330
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2021-12-05 10:27:44 +0530

    fix

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function calculateChange(amountToBeReturned){
     for(let i = 0; i < availableNotes.length; i++){
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
     
-        amountToBeReturned %= availableNotes[i];
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+       
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
