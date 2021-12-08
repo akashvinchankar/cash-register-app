0ad3a9d73bfa3c05d30c89fb83f788681914b22b
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2021-12-08 19:59:57 +0530

    minor fix

--- script.js
+++ script.js
@@ -7,34 +7,27 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click",function validateBillAmount(){
-    hideMessage();
-    if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+    
+    if(Number(billAmount.value) > 0){
+        message.innerText = "";
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
+            const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
             calculateChange(amountToBeReturned);
         }else{
-            showMessage("Do you want to wash plates?");
+            message.innerText = "Do you want to wash plates?";
         }
     }else{
-        showMessage("Invalid Bill Amount")       
+        message.innerText = "Invalid Bill Amount";      
     }
 });
 
-function calculateChange(amountToBeReturned){
+function calculateChange(val){
     for(let i = 0; i < availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        const numberOfNotes = Math.trunc(val / availableNotes[i]);
     
-        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        val %= availableNotes[i];
        
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
 
-function hideMessage(){
-    message.style.display = "none";
-}
-
-function showMessage(msg){
-    message.style.display = "block";
-        message.innerText = msg;
-}
